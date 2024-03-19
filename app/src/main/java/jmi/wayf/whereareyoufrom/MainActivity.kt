c11d254ba0c6d846b62e0440f973b5bc2f06d62f
jung0115 <jungim7490@gmail.com>
2024-03-19 21:52:00 +0900

    [FEAT] res/layout 폴더 나누기

--- app/src/main/java/jmi/wayf/whereareyoufrom/MainActivity.kt
+++ app/src/main/java/jmi/wayf/whereareyoufrom/MainActivity.kt
@@ -2,10 +2,23 @@ package jmi.wayf.whereareyoufrom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jmi.wayf.whereareyoufrom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // ViewBinding Setting
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // ViewBinding
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
